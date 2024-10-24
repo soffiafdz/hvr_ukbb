2f93aad33d5c4e0dac1ff0e287c0851db7cf8143
sfernandez <sofia.fernandezlozano@mail.mcgill.ca>
2024-10-24 15:43:11 -0400

    Finished comparison of xfms

--- code/filter_qc.R
+++ code/filter_qc.R
@@ -6,30 +6,68 @@ library(stringr)
 
 ### IN
 fpaths      <- c("lists/darq_out_20240408_lng_r152.csv",
-                 "data/arrow/UKBB_assemblynet_20240521.arrow") |> here()
+                 "data/arrow/UKBB_assemblynet_20240521.arrow",
+                 "data/xfm_distances_reza_vlad-lng.csv",
+                 "data/xfm_distances_vlad_v1-v2.csv") |> here()
 
 for (fpath in fpaths) {
   if (!file.exists(fpath)) {
     sprintf("File: %s is required but could not be found.", fpath) |> stop()
   }
 }
+rm(fpath)
 
+## DARQ (Automatic Reg QC)
+## This is useless
 darq.dt     <- fread(fpaths[1])
 darq.dt[, EID := as.integer(str_extract(fn, "(?<=-)\\d{7}"))]
 darq.dt[, INSTANCE := str_extract(fn, "ses-\\d")]
 darq.dt[, fn := NULL]
 darq.dt |> setcolorder(2:3) |> setnames("darq", "DARQ")
 
+## AssemblyNet (Automatic Reg QC)
+## Specific to ASSBLYNET data
 asnet.dt    <- arrow::read_feather(fpaths[2]) |>
 setDT() |>
 {\(DT) DT[, c(1:2, 8)]}() |>
 setnames(c("EID", "INSTANCE", "ASBLYNET"))
 asnet.dt[, EID := as.integer(str_remove(EID, "sub-"))]
 
-qc.dt       <- asnet.dt[darq.dt, on = .(EID, INSTANCE)]
-rm(fpaths, darq.dt, asnet.dt)
+## Comparison of xfms
+xfm_rv.dt   <- fread(fpaths[3], col.names = c("EID", "DIST_rv"), key = "EID")
+xfm_vv.dt   <- fread(fpaths[4], col.names = c("EID", "DIST_vv"), key = "EID")
 
-## OUT
+xfm.dt      <- xfm_vv.dt[xfm_rv.dt] |>
+melt(id = "EID", variable = "COMP", value = "DIST") |>
+{function(DT)
+  DT[, COMP := factor(COMP, labels = c("Vlad's pipeline: v1-v2",
+                                       "Pipelines: Reza's-Vlad's"))]}()
+
+# Plot histograms to compare distributions of distances
+xfm_q.dt    <- xfm.dt[,
+                      quantile(DIST, probs = c(.25, .5, .75), na.rm = T),
+                      COMP]
+p1          <- ggplot(xfm.dt[DIST < 10], aes(x = DIST)) +
+  theme_classic(base_size = 12) +
+  geom_histogram(binwidth = 0.05, fill = "transparent", colour = "black") +
+  geom_vline(data = xfm_q.dt, colour = "red", lty = "dashed",
+             aes(xintercept = V1)) +
+  facet_grid(rows = vars(COMP)) +
+  labs(title = "Comparison of transformations to stx space:",
+       x = "Frobenius norm", y = "Count",
+       caption = paste("Histograms are capped to 10.",
+                       "Dashed red lines indicate 25th, 50th & 75th quartiles",
+                       "on the complete data."))
+
+here("plots/comps_xfm.png") |>
+png(width = 12, height = 7, units = "in", res = 600)
+print(p1)
+dev.off()
+
+qc.dt       <- asnet.dt[darq.dt, on = .(EID, INSTANCE)]
+#rm(fpaths, darq.dt, asnet.dt)
+
+### OUT
 outrds      <- here("data/rds/qc_darq-assemblynet.rds")
 qc.dt |> saveRDS(outrds)
 rm(outrds)
--- code/link_vlad-xfm.sh
+++ code/link_vlad-xfm.sh
@@ -8,24 +8,27 @@ HERE=/ipl/ipl27/sfernandez/hvr_ukbb
 PPDIR=${HERE}/data/pp-vlad_t1
 PPLNGDIR=${HERE}/data/pp-vlad_lng_t1
 DESTDIR=${HERE}/data/pp-xfms_vlad
+LIST_NOTSUB=${HERE}/lists/missingv2fromv1_sub.txt
+LIST_NOTXFM=${HERE}/lists/missingv2fromv1_xfm.txt
+
+# Reinit lists
+> $LIST_NOTSUB
+> $LIST_NOTXFM
 
 # Loop through Vlad's files
-subdirs=($(fd -d 1 -t "d" "sub" "$PPDIR"))
-printf "%s\n" ${subdirs[@]} | \
-	pv -l -s ${#subdirs[@]} | \
-	while read -r subdir
+fd -d 2 "xfm" "$PPDIR" |
+	# There are 35350 xfm files
+	pv -l -s 35350 | \
+	while read -r xfm_v1
 	do
-		sub=$(basename $subdir)
-		# Check that xfm exists
-		xfm_v1=$(fd -d 1 "xfm" "$subdir")
-		[ -z $xfm_v1 ] && continue
+		sub=$(basename $(dirname $xfm_v1))
 
 		# Check that same subject was preprocessed by the lng pp
 		printf -v lngdir "%s/%s" $PPLNGDIR $sub
-		[ -d $lngdir ] || continue
+		[ ! -d $lngdir ] && echo $sub >> $LIST_NOTSUB && continue
 
 		xfm_v2=$(fd -d 1 "ses-2_t1.xfm" "$lngdir")
-		[ -z $xfm_v2 ] && continue
+		[ -z $xfm_v2 ] && echo $sub >> $LIST_NOTXFM && continue
 
 		# Create outdirectory
 		printf -v outdir "%s/%s" $DESTDIR $sub
