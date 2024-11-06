4c07050c257c8728be4112cd4fbc1e924c67e1dd
sfernandez <sofia.fernandezlozano@mail.mcgill.ca>
2024-11-06 12:24:46 -0500

    Final formatting

--- code/calc_sex-diff.R
+++ code/calc_sex-diff.R
@@ -11,18 +11,7 @@ library(ggtext)
 ### REMAKE plots
 REMAKE_PLOTS  <- TRUE
 
-### Sample size for density plots
-SAMPLE_N      <- 500
-
 ### INPUT
-## Covariates
-fpath         <- here("data/arrow/UKBB_covars_all.arrow")
-if (file.exists(fpath)) {
-  covars.dt   <- arrow::read_feather(fpath)
-} else {
-  here("code/parse_covars.R") |> source()
-}
-
 ## Adjusted HC/HVR
 fpath         <- here("data/rds/hc-hvr_adj.rds")
 if (file.exists(fpath)) {
@@ -30,8 +19,6 @@ if (file.exists(fpath)) {
 } else {
   here("code/adjust_hc-hvr.R") |> source()
 }
-
-rm(fpath)
 
 ### Data CLEANING
 ## Labels
@@ -111,30 +98,25 @@ cbPalette     <- c(
 ## Prepare data
 segm.str      <- names(hc_hvr_scl.lst)
 mtch.str      <- names(hc_hvr_scl.lst[[1]])
-def_caption   <- sprintf("%s %s\n%s %s",
-  "Data is scaled and centered.",
-# TODO: change sample size to be a variable?
-  sprintf(
-    "Densities are calculated from a random sample of %i subjects per group.",
-    SAMPLE_N
-  ),
-  "Dashed coloured lines represent the sample's means;",
+def_caption   <- sprintf("%s %s %s %s",
+  "N: %i Males & %i Females. Data is scaled and centered.",
+  "Solid lines are the regression slopes,",
+  "dashed coloured lines show the group means, and",
   "dashed gray lines represent a 1:1 correlation."
 )
 
 metadata.dt   <-
-  CJ(c("ICC", "HC_HVR", "HVR", "VC"), c("SIMP", "DET"), segm.str, mtch.str) |>
-  setnames(c("LIST", "PLOT", "SEGM", "MTCH")) |>
+  CJ(c("HC_HVR", "HVR", "VC"), segm.str, mtch.str) |>
+  setnames(c("LIST", "SEGM", "MTCH")) |>
   {function(DT) {
     DT[
       ,
       let(
-        FNAME = sprintf("effs-sex_%s_%s_%s_%s.png", LIST, SEGM, MTCH, PLOT) |>
+        FNAME = sprintf("effs-sex_%s_%s_%s.png", LIST, SEGM, MTCH) |>
           tolower(),
         TITLE = sprintf(
           "Sex differences: %s",
           fcase(
-            LIST == "ICC", "Intracranial volume",
             LIST %like% "HC", "Hippocampus and HC-to-VC Ratio",
             LIST == "HVR", "Conventional vs Residuals-adjusted HVR",
             LIST == "VC", "Ventricles"
@@ -150,46 +132,39 @@ metadata.dt   <-
         ),
         CAPTION = fcase(
           LIST == "HVR" & SEGM %like% "NET", sprintf(
-            "Notes: %s\n%s\n%s\n%s",
+            "Notes: %s %s %s %s",
+            "Validation analysis with AssemblyNet.",
             "HC-to-VC ratios are calculated from unadjusted (conventional)",
-            "or size-adjusted volumes by the residuals method (residuals).",
-            "Preprocessing & segmentation done with AssemblyNet.",
+            "or size-adjusted (residuals) volumes.",
             def_caption
           ),
           LIST == "HVR", sprintf(
-            "Notes: %s\n%s\n%s",
+            "Notes: %s %s %s",
             "HC-to-VC ratios are calculated from unadjusted (conventional)",
-            "or size-adjusted volumes by the residuals method (residuals).",
+            "or size-adjusted (residuals) volumes.",
             def_caption
           ),
           SEGM %like% "NET", sprintf(
-            "Notes: %s\n%s",
-            "Preprocessing & segmentation done with AssemblyNet.",
+            "Notes: %s %s",
+            "Validation analysis with AssemblyNet.",
             def_caption
           ),
-          default = sprintf("Notes: %s", def_caption)
+          default = paste("Notes:", def_caption)
         ),
-        HEIGHT = fcase(
-          PLOT == "SIMP", 3,
-          LIST == "HC_HVR", 8,
-          LIST == "HVR", 6,
-          LIST == "VC", 7
+        CAPTION_w = fcase(
+          LIST == "HC_HVR", 150,
+          LIST == "HVR", 115,
+          LIST == "VC", 130
         ),
-        WIDTH = fcase(
-          LIST == "ICC", 5,
+        SIZE = fcase(
           LIST == "HC_HVR", 8,
           LIST == "HVR", 6,
           LIST == "VC", 7
         )
       )
-      ][
-        !(LIST == "ICC" & (PLOT == "DET" | MTCH == "MTCH"))
-      ][
-        "ICC",
-        on = "LIST",
-        let(MTCH = NA, FNAME = sub("_all_simp", "", FNAME))
-      ]
+    ]
   }}()
+
 
 plot_dt.lst   <-
   # Parse Segmentation levels (CNN vs AssemblyNet)
@@ -197,31 +172,12 @@ plot_dt.lst   <-
     # Parse Cohorts levels (All vs Matched sample)
     Map(function(id2, DT1, DT2) {
       list(
-        # Lists for ICC comparisons
-        # When using, will have to unlist to compare All v Matched
-        ICC = list(
-          # Metadata for the plot
-          METADATA = metadata.dt[
-            SEGM == id1 & LIST == "ICC",
-            .(PLOT, TITLE, SUBTITLE, CAPTION, FNAME, HEIGHT, WIDTH)
-          ],
-          # Effect size labels for plots
-          EFFS = DT1[
-            rois["ICC"], on = "ROI"
-          ][
-            , .(VAR = id2, LABEL)
-          ],
-          # Individual data points
-          DATA = DT2[
-            rois["ICC"], on = "ROI", .(VAR = id2, VAL), by = .(EID, SEX)
-          ]
-        ),
         # Lists for HC & HVR comparisons
         HC_HVR = list(
           # Metadata for the plot
           METADATA = metadata.dt[
             SEGM == id1 & MTCH == id2 & LIST == "HC_HVR",
-            .(PLOT, TITLE, SUBTITLE, CAPTION, FNAME, HEIGHT, WIDTH)
+            .(TITLE, SUBTITLE, CAPTION, CAPTION_w, FNAME, SIZE)
           ],
           # Effect size labels for plots
           EFFS = DT1[
@@ -259,7 +215,7 @@ plot_dt.lst   <-
           # Metadata for the plot
           METADATA = metadata.dt[
             SEGM == id1 & MTCH == id2 & LIST == "HVR",
-            .(PLOT, TITLE, SUBTITLE, CAPTION, FNAME, HEIGHT, WIDTH)
+            .(TITLE, SUBTITLE, CAPTION, CAPTION_w, FNAME, SIZE)
           ],
           # Effect size labels for plots
           EFFS = DT1[
@@ -294,7 +250,7 @@ plot_dt.lst   <-
           # Metadata for the plot
           METADATA = metadata.dt[
             SEGM == id1 & MTCH == id2 & LIST == "VC",
-            .(PLOT, TITLE, SUBTITLE, CAPTION, FNAME, HEIGHT, WIDTH)
+            .(TITLE, SUBTITLE, CAPTION, CAPTION_w, FNAME, SIZE)
           ],
           # Effect size labels for plots
           EFFS = DT1[
@@ -329,66 +285,35 @@ plot_dt.lst   <-
   }, segm.str, effs.lst, hc_hvr_scl.lst
 )
 
-# Extract ICC from All & Matched and merge them:
-plot_dt.lst   <- lapply(plot_dt.lst, function(sublist) {
-  sublist1    <- sublist[[1]][[1]]
-  sublist2    <- sublist[[2]][[1]]
-  list_names  <- names(sublist1)
-  # Create a new "ICC" sublist at the 2nd level
-  sublist[["ICC"]] <- lapply(list_names, function(name) {
-    # METADATA is the same for both ALL & MTCH levels
-    if (name == "METADATA") {
-      sublist1[[name]]
-    } else {
-      rbind(sublist1[[name]], sublist2[[name]]) |>
-      {function(DT) {
-        DT[, VAR := factor(VAR, labels = c("General", "Matched"))]
-      }}()
-    }
-  })
-
-  # Rename
-  names(sublist[["ICC"]]) <- list_names
-
-  #Delete originals
-  sublist[[1]][[1]] <- sublist[[2]][[1]] <- NULL
-
-  # Return
-  sublist
-})
-
-
 ## GGally custom functions
 # Diagonal
-diag_fun      <- function(data, subdata, mapping, var, effs_labs, ...) {
-  ggplot(mapping = mapping) +
-    #geom_histogram(colour = "transparent", alpha = .25) +
-    # Calculate density with a subsample of the data for cheaper processing
-    geom_density(data = subdata, alpha = .1) +
-    # For mean lines, use the complete data
-    stat_summary(
-      aes(xintercept = ..x.., y = 0),
-      data = data,
-      fun = mean,
-      geom = "vline",
-      orientation = "y",
-      linetype = "dashed",
-      linewidth = .4,
-      alpha = .75
-    ) +
-    # Effect size data is obtained from a different data.table
-    geom_richtext(
-      aes(label = LABEL),
-      data = effs_labs[effs_labs$VAR == as_label(mapping$x)],
-      size = 2.3,
-      alpha = .7,
-      colour = "Black",
-      fill = "White",
-      x = -Inf,
-      y = -Inf,
-      hjust = -0.1,
-      vjust = -0.25
-    )
+diag_fun      <- function(data, mapping, var, effs_labs, ...) {
+  # Calculate density first to get the y-axis limits
+  p <- ggplot(mapping = mapping) + geom_density(data = data, alpha = .1)
+  y_limits <- ggplot_build(p)$layout$panel_scales_y[[1]]$range$range
+  # Add mean lines (complete data) and labels (effs_labs)
+  p + stat_summary(
+    aes(xintercept = ..x.., y = 0),
+    data = data,
+    fun = mean,
+    geom = "vline",
+    orientation = "y",
+    linetype = "dashed",
+    linewidth = .4,
+    alpha = .75
+  ) + geom_richtext(
+    aes(label = LABEL),
+    data = effs_labs[effs_labs$VAR == as_label(mapping$x)],
+    size = 2.3,
+    #alpha = .7,
+    colour = "Black",
+    fill = "White",
+    x = -Inf,
+    y = y_limits[2] + 0.08,
+    hjust = -0.2,
+    #vjust = 0.25
+  ) + scale_y_continuous(limits = c(y_limits[1], y_limits[2] + 0.13))
+
 }
 
 # Lower
@@ -400,111 +325,31 @@ lower_fun     <- function(data, mapping, ...) {
       slope = 1,
       lty = "dashed",
       colour = cbPalette[1]
-    )
+    ) |> invisible()
 }
 
-simp_plot     <- function(
-  dataset,
-  effs_labs,
-  metadata,
-  sample_n = 100
-) {
-  fplot       <- here("plots", metadata["SIMP", on = "PLOT", FNAME])
-  if (any(!file.exists(fplot), REMAKE_PLOTS)) {
-    time      <- system.time({
-      p       <- dataset[, .SD[sample(.N, min(.N, sample_n))], .(SEX, VAR)] |>
-        ggplot(aes(x = VAL, colour = SEX, fill = SEX)) +
-        theme_classic(base_size = 10) +
-        theme(
-          text = element_text(size = 10),
-          axis.text.y = element_blank(),
-          axis.ticks.y = element_blank(),
-          axis.line.y = element_blank(),
-          legend.position = "bottom"
-        ) +
-        scale_fill_manual(values = cbPalette[c(8, 6)]) +
-        scale_colour_manual(values = cbPalette[c(8, 6)]) +
-        #geom_histogram(fill = "transparent", bins = 45) +
-        geom_density(alpha = .1) +
-        geom_vline(
-          aes(xintercept = VAL, colour = SEX),
-          data = dataset[, lapply(.SD, mean), .(SEX, VAR)],
-          lty = "longdash",
-          linewidth = .5,
-          alpha = .75
-        ) +
-        facet_wrap(vars(VAR), nrow = 1) +
-        geom_richtext(
-          aes(label = LABEL),
-          data = effs_labs,
-          size = 2,
-          alpha = .8,
-          colour = "Black",
-          fill = "White",
-          #x = -Inf, y = -Inf, hjust = -0.1, vjust = -0.25
-          vjust = -.2,
-          x = 0,
-          y = 0
-        ) +
-        labs(
-          title = metadata["SIMP", on = "PLOT", TITLE],
-          subtitle = if (is.na(metadata["SIMP", on = "PLOT", SUBTITLE])) {
-            NULL
-          } else {
-            metadata["SIMP", on = "PLOT", SUBTITLE]
-          },
-          caption = metadata["SIMP", on = "PLOT", CAPTION],
-          x = NULL,
-          y = NULL,
-          colour = "Sex",
-          fill = "Sex"
-        )
-    })
 
-    # Save time for reference
-    metadata["SIMP", on = "PLOT", TIME := time[3]]
-
-    # Save plot
-    #browser()
-    ggsave(
-      filename = fplot,
-      plot = p,
-      height = metadata["SIMP", on = "PLOT", HEIGHT],
-      width = metadata["SIMP", on = "PLOT", WIDTH],
-      units = "in",
-      dpi = 600
-    ) |>
-    invisible()
-  }
-}
-
-det_plot      <- function(
-  dataset,
-  columns,
-  effs_labs,
-  metadata,
-  sample_n = 100
-) {
-  fplot       <- here("plots", metadata["DET", on = "PLOT", FNAME])
+det_plot      <- function(dataset, columns, effs_labs, metadata) {
+  fplot       <- here("plots", metadata$FNAME)
   if (any(!file.exists(fplot), REMAKE_PLOTS)) {
     time      <- system.time({
       dataset <- dcast(dataset, ... ~ VAR, value.var = "VAL")
-      subdata <- dataset[, .SD[sample(.N, min(.N, sample_n))], SEX]
+      n_f     <- dataset["Female", on = "SEX", .N]
+      n_m     <- dataset["Male", on = "SEX", .N]
+      caption <- metadata$CAPTION |>
+        sprintf(n_m, n_f) |>
+        stringr::str_wrap(metadata$CAPTION_w, exdent = 10)
       p       <-  dataset |>
         setcolorder(rois[["ICC"]], after = "SEX") |>
         ggpairs(
           aes(colour = SEX, fill = SEX),
           columns = columns,
           lower = list(continuous = lower_fun),
-          diag = list(continuous = wrap(
-            diag_fun,
-            subdata = subdata,
-            effs_labs = effs_labs
-          ))
+          diag = list(continuous = wrap(diag_fun, effs_labs = effs_labs))
         ) +
         theme_classic(base_size = 10) +
         theme(
-          text = element_text(size = 10),
+          text = element_text(size = 9),
           #axis.text.y = element_blank(),
           #axis.ticks.y = element_blank(),
           #axis.line.y = element_blank(),
@@ -513,13 +358,13 @@ det_plot      <- function(
         scale_fill_manual(values = cbPalette[c(8, 6)]) +
         scale_colour_manual(values = cbPalette[c(8, 6)]) +
         labs(
-          title = metadata["DET", on = "PLOT", TITLE],
-          subtitle = if (is.na(metadata["DET", on = "PLOT", SUBTITLE])) {
+          title = metadata$TITLE,
+          subtitle = if (is.na(metadata$SUBTITLE)) {
             NULL
           } else {
-            metadata["DET", on = "PLOT", SUBTITLE]
+            metadata$SUBTITLE
           },
-          caption = metadata["DET", on = "PLOT", CAPTION],
+          caption = caption,
           #x = NULL,
           #y = NULL,
           colour = "Sex",
@@ -528,14 +373,14 @@ det_plot      <- function(
     })
 
     # Save time for reference
-    metadata["DET", on = "PLOT", TIME := time[3]]
+    metadata[, TIME := time[3]]
 
     # Save plot
     ggsave(
       filename = fplot,
       plot = p,
-      height = metadata["DET", on = "PLOT", HEIGHT],
-      width = metadata["DET", on = "PLOT", WIDTH],
+      height = metadata$SIZE,
+      width = metadata$SIZE,
       units = "in",
       dpi = 600
     ) |>
@@ -547,45 +392,23 @@ det_plot      <- function(
 lapply(plot_dt.lst, function(match_lvl) {
   lapply(names(match_lvl), function(sublist_name) {
     sublist <- match_lvl[[sublist_name]]
-    if (sublist_name == "ICC") {
-      # Plot simple comparison plots for only ICC
-      sublist   <- match_lvl[[sublist_name]]
+    lapply(match_lvl[[sublist_name]], function(sublist) {
+      # Sublists
       dataset   <- sublist[["DATA"]]
       effs_labs <- sublist[["EFFS"]]
       metadata  <- sublist[["METADATA"]]
-      simp_plot(
+      vars      <- dataset[, unique(VAR)]
+
+      ## Detailed plots
+      ## Comparison plots between adjustment methods
+      det_plot(
         dataset = dataset,
-        sample_n = SAMPLE_N,
+        columns = vars,
         effs_labs = effs_labs,
         metadata = metadata
       )
-    } else {
-      lapply(match_lvl[[sublist_name]], function(sublist) {
-        # Sublists
-        dataset   <- sublist[["DATA"]]
-        effs_labs <- sublist[["EFFS"]]
-        metadata  <- sublist[["METADATA"]]
-        vars      <- dataset[, unique(VAR)]
-
-        ## Simple plots
-        ## Only density plots with median lines
-        simp_plot(
-          dataset = dataset,
-          sample_n = SAMPLE_N,
-          effs_labs = effs_labs,
-          metadata = metadata
-        )
-
-        ## Detailed plots
-        ## Comparison plots between adjustment methods
-        det_plot(
-          dataset = dataset,
-          sample_n = SAMPLE_N,
-          columns = vars,
-          effs_labs = effs_labs,
-          metadata = metadata
-        )
-      })
-    }
+    })
   })
 })
+
+rm(fpath, def_caption, cbPalette, adjs, rois, mtch.str, segm.str)
